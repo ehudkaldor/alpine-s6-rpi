43d6ca29f6c21d1c4840649a029f60896592fd2e
Ehud Kaldor <ehud.kaldor@gmail.com>
2016-03-21 23:44:09 -0700

    fixed to look for all containers in the stack, and get 'ip hostname hostname.domainname name' to /tmp/hosts

--- rootfs/bin/get_hosts_from_dockercloud.sh
+++ rootfs/bin/get_hosts_from_dockercloud.sh
@@ -1,27 +1,57 @@
-echo "running /bin/get_hosts_from_dockercloud.sh"
 parseServiceLinks() {
-    while read ip service_link host
+  while read service_uri
+  do
+    CONTAINERS=$(curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_REST_HOST}${service_uri} | jq -r ".containers[]")
+    echo "container URIs: $CONTAINERS"
+    for CONT_URI in $CONTAINERS
     do
-#        curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_SERVICE_API_URL}${service_link} > /tmp/cur_service
-#        echo "curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_SERVICE_API_URL}${service_link} yielded " `cat /tmp/cur_service`
-        curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_SERVICE_API_URL} > /tmp/cur_service
-        echo "curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_SERVICE_API_URL} yielded " `cat /tmp/cur_service`
-        fqdn=$(cat /tmp/cur_service | jq -r '.public_dns')
-        echo "fqdn: $fqdn"
-        host=$(cat /tmp/cur_service  | jq -r '.name')
-        echo "host: $host"
-        echo "${ip} ${host} ${fqdn}" >> /tmp/hosts
+      ANS=$(curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_REST_HOST}${CONT_URI})
+      export LINE
+      if [[ -n $(echo $ANS | jq -r '.domainname') && $(echo $ANS | jq -r '.domainname') != 'null' ]]
+      then 
+        LINE=$(echo $ANS | jq -r '. | "\(.private_ip) \(.hostname) \(.hostname).\(.domainname)"')
+      else
+        LINE=$(echo $ANS | jq -r '. | "\(.private_ip) \(.hostname)"') 
+      fi
+      if [[ $(echo $ANS | jq -r '.hostname') != $(echo $ANS | jq -r '.name') ]]
+      then
+        LINE="$LINE $(echo $ANS | jq -r '.name')"
+      fi
+      echo $LINE >> /tmp/hosts
     done
-    echo "content of /tmp/hosts: " `cat /tmp/hosts`
+  done
 }
 
-
-#if [ -n "${DOCKERCLOUD_AUTH}" ] && [ -n "${TUTUM_API_CALLS_FOR_DNS}" ]
 if [ -n "${DOCKERCLOUD_AUTH}" ] 
 then
-    curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_REST_HOST}/api/app/v1/container/ > /tmp/containers.raw
-    echo "curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_REST_HOST}/api/app/v1/container/ yielded " `cat /tmp/containers.raw`
-    cat /tmp/containers.raw | jq -r '.objects  | map ( "\(.private_ip) \(.name) \(.public_dns)" ) | .[]' | tr -d '"' >> /tmp/hosts
-    echo "content of /tmp/hosts: " `cat /tmp/hosts`
-    cat /tmp/containers.raw | jq -r '.objects  | map ( "\(.private_ip) \(.service) \(.name)" ) | .[]' | tr -d '"' | parseServiceLinks
+  STACKS=$(curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_REST_HOST}/api/app/v1/stack/ | jq -r '.objects[]')
+  export FOUND=false
+  for STACK in "${STACKS[@]}"
+  do
+    STACK_URI=$(echo $STACK | jq -r '.resource_uri')
+    if [ -n $(echo $STACK | jq -r '.services[] | select(. | contains ("${DOCKERCLOUD_SERVICE_API_URI}"))') ]
+    then
+      FOUND=true
+      echo $STACK | jq -r '.services[]' | parseServiceLinks
+    fi
+  done
+  if [ ! FOUND ]
+  then
+    echo "My service uri not found in any stack"
+  fi
+
+
+
+#  SERVICES=$(curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_REST_HOST}/api/app/v1/stack/ | jq -r ".objects[0] | .services[]")
+#  echo "services URIs: $SERVICES"
+#  for SRV_URI in $SERVICES
+#  do
+#    CONTAINERS=$(curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_REST_HOST}${SRV_URI} | jq -r ".containers[]")
+#    echo "container URIs: $CONTAINERS"
+#    for CONT_URI in $CONTAINERS
+#    do
+#      CONT_DETAILS=$(curl -s -H "Authorization: $DOCKERCLOUD_AUTH" -H "Accept: application/json" ${DOCKERCLOUD_REST_HOST}${CONT_URI} | jq -r ".hostname, .domainname, .private_ip, .name")
+#      echo "details: $CONT_DETAILS"
+#    done
+#  done
 fi

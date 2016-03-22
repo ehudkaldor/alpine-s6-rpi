a00d209751c772fd73f411a309f63827280fa460
Ehud Kaldor <ehud.kaldor@gmail.com>
2016-03-21 23:44:39 -0700

    changed TUTUM to DOCKERCLOUD

--- rootfs/bin/dockercloud_dns_hack.sh
+++ rootfs/bin/dockercloud_dns_hack.sh
@@ -1,12 +1,12 @@
 echo "running /bin/dockercloud_dns_hack.sh"
-env_vars=$(env | grep "_ENV_TUTUM_IP_ADDRESS=" | cut -d= -f1 | tr '\n' ' ' )
+env_vars=$(env | grep "_ENV_DOCKERCLOUD_IP_ADDRESS=" | cut -d= -f1 | tr '\n' ' ' )
 echo "env_vars: $env_vars"
 echo "#Auto Generated - DO NOT CHANGE" >> /tmp/hosts
 echo "looping on env_vars"
 for env_var in $env_vars
 do
     echo "env_var: $env_var"
-    host=$(echo $env_var | awk -F"_ENV_TUTUM_IP_ADDRESS" '{print $1;}' | tr '_' '-' | tr '[:upper:]' '[:lower:]' )
+    host=$(echo $env_var | awk -F"_ENV_DOCKERCLOUD_IP_ADDRESS" '{print $1;}' | tr '_' '-' | tr '[:upper:]' '[:lower:]' )
     echo "host: $host"
     ip=$(eval "echo \$$env_var" | cut -d/ -f1)
     echo "ip: $ip"

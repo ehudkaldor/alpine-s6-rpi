fe9faa2f16dc19404d8cba72696399923bf5e111
Ehud Kaldor <ehud.kaldor@gmail.com>
2016-10-10 21:38:32 -0700

    moved outside of rootfs, to disable dns hack

--- bin/additional_hosts.sh
+++ bin/additional_hosts.sh
@@ -8,12 +8,12 @@ then
     cont1=$(echo $DOCKERCLOUD_CONTAINER_FQDN | cut -d'.' -f2-)
     cont2=$(echo $DOCKERCLOUD_CONTAINER_FQDN | cut -d'.' -f3-)
 
-    echo "nameserver: $nameserver"
-    echo "search: $search"
-    echo "service1: $service1"
-    echo "service2: $service2"
-    echo "cont1: $cont1"
-    echo "cont2: $cont2"
+#    echo "nameserver: $nameserver"
+#    echo "search: $search"
+#    echo "service1: $service1"
+#    echo "service2: $service2"
+#    echo "cont1: $cont1"
+#    echo "cont2: $cont2"
 
     echo "running loop on BA_ADDITIONAL_HOSTS ($BA_ADDITIONAL_HOSTS)"
     for host in $BA_ADDITIONAL_HOSTS
